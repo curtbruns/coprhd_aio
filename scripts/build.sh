7c2bdad0b231780c0e73b6dcfcdbaa7ffa55193d
Curt Bruns <curt.e.bruns@intel.com>
2016-02-09 11:18:25 -0700

    Updated to use Master branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -42,9 +42,6 @@ if [ "$build" = true ] || [ ! -e /vagrant/*.rpm ]; then
   cd /tmp
   git clone https://github.com/CoprHD/coprhd-controller.git
   cd coprhd-controller
-  # Change to Feature Branch
-  git checkout -b feature-keystone-auto-reg origin/feature-keystone-auto-reg
-  cd /tmp/coprhd-controller
   make clobber BUILD_TYPE=oss rpm
   rm -rf /vagrant/*.rpm
   cp -a /tmp/coprhd-controller/build/RPMS/x86_64/storageos-*.x86_64.rpm /vagrant
